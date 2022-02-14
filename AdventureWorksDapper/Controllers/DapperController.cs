590daab2e6254a85db137f61becbe355b7680c2d
thomasfregene <fregzythomas@gmail.com>
2022-02-14 09:59:28 +0100

    Add project files.

---
+++ AdventureWorksDapper/Controllers/DapperController.cs
@@ -0,0 +1,38 @@
+﻿using AdventureWorksDapper.Model;
+using Dapper;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AdventureWorksDapper.Controllers
+{
+    [ApiController]
+    [Route("api/dapper")]
+    public class DapperController : Controller
+    {
+        private const string CONNECTION_STRING = "Server=HICAD-TOM\\SQLEXPRESS;Database=AdventureWorks;Trusted_Connection=True;";
+
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            var sql = @"SELECT 
+                          [PersonType]
+                          ,[NameStyle]
+                          ,[Title]
+                          ,[FirstName]
+                          ,[MiddleName]
+                          ,[LastName]
+                      FROM [Person].[Person]
+                      WHERE FirstName=@firstName";
+            using (var connection = new SqlConnection(CONNECTION_STRING))
+            {
+                var persons = await connection.QueryAsync<Person>(sql, new { firstName="Kelvin"});
+                return Ok(persons);
+            }
+            //return Ok();
+        }
+    }
+}
---
+++ AdventureWorksDapper/Model/Person.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AdventureWorksDapper.Model
+{
+    public class Person
+    {
+        public string Title { get; set; }
+        public string FirstName { get; set; }
+        public string MiddleName { get; set; }
+        public string LastName { get; set; }
+
+    }
+}
