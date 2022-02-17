5eab0dd7370cba8a978637ae03df11ef3ba73e0e
thomasfregene <fregzythomas@gmail.com>
2022-02-17 08:58:29 +0100

    INSERT QUERY

--- AdventureWorksDapper/Controllers/DapperController.cs
+++ AdventureWorksDapper/Controllers/DapperController.cs
@@ -16,23 +16,71 @@ namespace AdventureWorksDapper.Controllers
         private const string CONNECTION_STRING = "Server=HICAD-TOM\\SQLEXPRESS;Database=AdventureWorks;Trusted_Connection=True;";
 
         [HttpGet]
-        public async Task<IActionResult> Index()
+        public async Task<IActionResult> Index([FromQuery]bool getKelvins)
         {
-            var sql = @"SELECT 
-                          [PersonType]
-                          ,[NameStyle]
-                          ,[Title]
-                          ,[FirstName]
-                          ,[MiddleName]
-                          ,[LastName]
-                      FROM [Person].[Person]
-                      WHERE FirstName=@firstName";
+            var sql = @"INSERT INTO [dbo].[TestTable]
+                           ([Foobar])
+                            VALUES
+                           (@foobar)";
+
             using (var connection = new SqlConnection(CONNECTION_STRING))
             {
-                var persons = await connection.QueryAsync<Person>(sql, new { firstName="Kelvin"});
-                return Ok(persons);
+                for (int x = 0; x < 1000; x++)
+                {
+                    await connection.ExecuteAsync(sql, new { foobar = $"testing {DateTime.UtcNow.Ticks}" });
+                }
+                return Ok();
             }
             //return Ok();
+        }
+
+      
+
+        private static async Task ExecuteAsyncWithInsert(string sqlConnection)
+        {
+            await using var connection = new SqlConnection(sqlConnection);
+            var sql = @"INSERT INTO [Person].[Person]
+                         ([Title]
+                          ,[FirstName]
+                          ,[MiddleName]
+                          ,[LastName])
+                          VALUES
+                           (@Title, @FirstName,@MiddleName,@LastName)";
+            var person = new Person
+            {
+                Title = "Mr.",
+                FirstName = "Jude",
+                LastName = "Akhimien",
+                MiddleName = "Odianose",
+            };
+            var result = connection.ExecuteAsync(sql, person);
+
+        }
+
+        private static async Task StoredProcedure(string connectionString)
+        {
+            await using var connection = new SqlConnection(connectionString);
+            var sqlProc = "dbo.uspGetBillOfMaterials";
+            var results = await connection.QueryAsync<string>(sqlProc, commandType: System.Data.CommandType.StoredProcedure);
+        }
+
+        private static async Task ExecuteAsyncWithUpdate(string connectionString)
+        {
+            await using var connection = new SqlConnection(connectionString);
+            var sql = @"Update [Person].[Person]
+                         SET Title = @Title
+                          WHERE FirstName=@firstName";
+
+            var result = await connection.ExecuteAsync(sql, new { title = "Mr.", firstName = "Jude" });
+        }
+
+        private static async Task ExecuteAsyncWithDelete(string connectionString)
+        {
+            await using var connection = new SqlConnection(connectionString);
+
+            var sql = @"DELETE FROM [Person].[Person]
+                          WHERE FirstName=@firstName";
+            var result = await connection.ExecuteAsync(sql, new { firstName = "Jude" });
         }
     }
 }
